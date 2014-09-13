0bf60f574cfaa8bbef84a6a99547b8dced9cc91d
elibtronic <tim.ribaric@gmail.com>
2014-09-13 19:55:18 -0400

    Soln found

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ csc_qotw
 ========
 
 Computer Science Club Question of the week
+
+hailstorm - attempted Sept 13,2014
--- hailstorm/hailstorm.py
+++ hailstorm/hailstorm.py
@@ -17,18 +17,20 @@ def hailstorm(n, h):
 
 
 h_long = []
-h_temp = []
 longest_seed = 0
 longest_chain = 0
 
-for n in range(3,4):
+#will stop at 999999
+for n in range(1,1000000):
+    h_temp = []
     hailstorm(n,h_temp)
     if len(h_temp) > longest_chain:
         h_long = []
         h_long = h_temp
         longest_seed = n
         longest_chain = len(h_temp)
-    h_temp = []
 
 print "Longest Chain Seed: ",longest_seed
+print "seq_len: ",longest_chain
 print "sequence: ",h_long
+
